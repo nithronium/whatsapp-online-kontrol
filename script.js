fed4c2d38463fc01cbfd9cc94d7cb58dcf5da44a
nithronium <45077058+nithronium@users.noreply.github.com>
2020-02-24 14:06:24 +0300

    yazıyor ve ses kaydı düzeltmeleri
    karşıdaki kişi yazıyor veya ses kaydediyor olduğunda bunu ayrı şekilde gösteriyor.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var ilkcevrim = "0";
 var soncevrim = "0";
+var ilkyaziyor = "0";
+var sonyaziyor = "0";
+var ilkses = "0";
+var sonses = "0";
 
 setInterval(function(){
     var d = new Date();
     d.toLocaleTimeString();
     var source = document.getElementsByTagName('html')[0].innerHTML;
     var foundIndex = source.search("çevrimiçi");
-    var foundYaziyor = source.search("yazıyor");
-    var foundSes = source.search("ses kayd");
+    var foundYaziyor = source.search('title="yazıyor');
+    var foundSes = source.search('title="ses kayd');
+
+    //Çevrimiçi kontrol
     if((foundIndex != -1) || (foundYaziyor != -1) || (foundSes !=-1)){
         if (ilkcevrim == "0"){
             ilkcevrim = d;
@@ -24,4 +30,43 @@ setInterval(function(){
             soncevrim = 0;
         }
     }
+
+
+    //Yazıyor kontrol
+    if (foundYaziyor != -1){
+        if (ilkyaziyor == "0"){
+            ilkyaziyor = d;
+        }
+        else {
+            sonyaziyor = d;
+        }
+    }
+    else {
+        if((ilkyaziyor !=0) && (sonyaziyor!=0)){
+            yaziyorzamani = ilkyaziyor + "'da yazmaya başladı ve " + sonyaziyor + "'da yazmayı bitirdi.";
+            console.log(yaziyorzamani);
+            ilkyaziyor = 0;
+            sonyaziyor = 0;
+        }
+    }
+
+
+    //Ses kaydediyor kontrol
+    if (foundSes != -1){
+        if (ilkses == "0"){
+            ilkses = d;
+        }
+        else {
+            sonses = d;
+        }
+    }
+    else {
+        if((ilkses !=0) && (sonses!=0)){
+            seszamani = ilkses + "'da yazmaya başladı ve " + sonses + "'da yazmayı bitirdi.";
+            console.log(seszamani);
+            ilkses = 0;
+            sonses = 0;
+        }
+    }
+
 },300);
